2694c21fd73f212d715b5798080effede976b006
Maximiliano Cambiaso <maxcambiaso@gmail.com>
2024-08-14 21:48:30 -0300

    IDK minor changes

--- eddie_wallet/lib/main.dart
+++ eddie_wallet/lib/main.dart
@@ -12,12 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Eddie Wallet',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Choose your account'),
     );
   }
 }
